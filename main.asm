0f91188ece66db9c9150533751b7564b1b5046ac
wardog1uk <wardog1uk@users.noreply.github.com>
2021-10-26 13:31:34 +0100

    Use compressed data for screen frame (#6)
    * Replace data with the compressed data
    
    * Extract function to move to top left
    
    * Output compressed data to screen

--- main.asm
+++ main.asm
@@ -34,25 +34,35 @@ setup
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
-; draw the screen frame
+; draw the screen frame from compressed data (must be <256 bytes long)
 drawFrame
-        ldx #250
-@loop   lda data-1,x
-        sta screenRam-1,x
-        lda data+249,x
-        sta screenRam+249,x
-        lda data+499,x
-        sta screenRam+499,x
-        lda data+749,x
-        sta screenRam+749,x
+        jsr moveToTopLeftOfScreen
+
+        ldy #$00
+@loop
+        lda compressed,y
+        beq @done
+
+        iny
+        sty counter
+
+        ldx compressed,y
+@xloop
+        jsr outputChar
         dex
-        bne @loop
+        bne @xloop
+
+        ldy counter
+        iny
+        jmp @loop
+@done
         rts
+
+counter BYTE $00
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
-; move to first line of data
-resetPosition
+moveToTopLeftOfScreen
         lda #<screenRam
         sta lineStart
         lda #>screenRam
@@ -60,6 +70,15 @@ resetPosition
 
         lda #$00
         sta row
+        sta col
+
+        rts
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; move to first line of data
+resetPosition
+        jsr moveToTopLeftOfScreen
 
         jsr moveToNextLine
         jsr moveToNextLine
