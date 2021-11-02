3c37ec51fa3139b8d9f7add0dd482c32be5f208e
wardog1uk <wardog1uk@users.noreply.github.com>
2021-11-02 15:34:05 +0000

    Invert first byte

--- edit.asm
+++ edit.asm
@@ -1,4 +1,32 @@
 ;-------------------------------------------------------------------------------
 showEditScreen
+        jsr moveToTopLeftOfScreen
+        jsr moveToNextLine
+        jsr moveToNextLine
+
+        ldy #$07
+        sty col
+
+        jsr invertEditValue
+
+@wait   jsr GETIN
+        beq @wait
+
+        sec
+        rts
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; invert characters at (linestart),y and (linestart),y+1
+invertEditValue
+        lda (lineStart),y
+        adc #$80
+        sta (lineStart),y
+
+        iny
+        lda (lineStart),y
+        adc #$80
+        sta (lineStart),y
+
         rts
 ;-------------------------------------------------------------------------------
